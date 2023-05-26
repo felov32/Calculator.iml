497bd6e11357d51626a81760bad8129917e70439
felov32 <134709295+felov32@users.noreply.github.com>
2023-05-26 16:05:34 +0300

    Update Result.java

--- src/Result.java
+++ src/Result.java
@@ -1,6 +1,8 @@
 public class Result {
+
     public static int calc(int number1, int number2, char operator) {
         int result = 0;
+
         switch (operator) {
             case '+':
                 result = number1 + number2;
@@ -20,15 +22,4 @@ public class Result {
         return result;
     }
 
-    static String answer(int result) throws Exception { // Вывод ответа
-        if (Calculator.isRome & result < 1) {
-            throw new Exception("В римской системе нет отрицательных чисел");
-        }
-        if (Calculator.isRome) {
-            System.out.println("Ответ " + Converter.convertArabToRoman(result));
-        } else {
-            System.out.println("Ответ: "  + result);
-        }
-        return null;
-    }
 }
