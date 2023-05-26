35105b00045932e06300586bd8eb13441085a249
felov32 <134709295+felov32@users.noreply.github.com>
2023-05-26 16:05:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Введиде выряжение для решения, формат 'a + b', через пробел.Допускаются числа от 1 до 10 или от I до X включительно:");
+        System.out.println("Введиде выряжение для решения, формат 'a + b', через пробел.Допускаются числа от 1 до 10 включительно:");
         String stringIn = new Scanner(System.in).nextLine().trim();
         calc(stringIn);
+        System.out.println(calc(stringIn));
     }
+
     public static String calc(String input) throws Exception {
 
         String[] inputs = Calculator.read(input);
         int[] num = Calculator.check(inputs);
         int result = Result.calc(num[0], num[1], (char) num[2]);
-        return Result.answer(result);
+        return String.valueOf(result);
 
     }
 
 }
-
 
